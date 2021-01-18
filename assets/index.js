3669b8bb3a4e7d7321c4bbdedea544251549adbb
Tomas Gomes <tvb.gomes@gmail.com>
2021-01-18 11:28:18 -0300

    Started Pimping my profile page with some cool JS

---
+++ assets/index.js
@@ -0,0 +1,15 @@
+
+
+fetch("https://api.github.com/users/tvbgo")
+  .then(response => response.json())
+  .then((data) => {
+    const myBio = document.querySelectorAll(".github-bio");
+    const bio = data.bio;
+    myBio[0].insertAdjacentHTML("afterbegin", bio);
+  });
+
+
+
+
+
+
