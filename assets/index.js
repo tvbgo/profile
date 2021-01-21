9d989fbe95a4109d98550c2da38cd8d6d4bbead3
Tomas Gomes <tvb.gomes@gmail.com>
2021-01-21 16:58:15 -0300

    refactored JS

--- assets/index.js
+++ assets/index.js
@@ -1,12 +1,14 @@
 
-
-fetch("https://api.github.com/users/tvbgo")
-  .then(response => response.json())
-  .then((data) => {
-    const myBio = document.querySelectorAll(".github-bio");
-    const bio = data.bio;
-    myBio[0].insertAdjacentHTML("afterbegin", bio);
-  });
+const getBio = () => {
+  fetch("https://api.github.com/users/tvbgo")
+    .then(response => response.json())
+    .then((data) => {
+      const myBio = document.querySelectorAll(".github-bio");
+      const bio = data.bio;
+      myBio[0].insertAdjacentHTML("afterbegin", bio);
+    });
+}
+getBio();
 
 
 
